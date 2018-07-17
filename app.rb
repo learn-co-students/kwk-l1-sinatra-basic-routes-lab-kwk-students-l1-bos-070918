8df773cefa4fc607eb7999c2936a20ae05931f18
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-17 14:04:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello, World!"
+  end
+  
+get'/name' do 
+  "My name is Julia"
+ end
+
+get '/hometown' do
+"My hometown is Brookline"
+end
+
+get '/favorite-song' do
+"My favorite song is god is biger than the bookie man(veggie tails)"
+end
+
 end
