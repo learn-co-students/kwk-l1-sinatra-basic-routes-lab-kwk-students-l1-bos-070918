77d84f80d97fe1cd76baa3d3dd988941305518ac
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-17 14:03:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World"
+  end
+  
+  get '/name' do
+    "My name is Pelumi"
+  end
+  
+  get '/hometown' do
+    "My hometown is Providence"
+  end
+  
+  get '/favourite-song' do
+    "My favourite-song is Gbese"
+  end
 end
