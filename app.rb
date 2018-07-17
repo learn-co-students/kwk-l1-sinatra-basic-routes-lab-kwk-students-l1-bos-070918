a9bbd7faf45a7ddc7494264fe8506076d092a9cf
alexagtn2714 <alexagaton@hotmail.com>
2018-07-17 15:44:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end 
+  
+  get '/name' do
+    "My name is Alexa"
+  end 
+  
+  get '/hometown' do
+  "My hometown is La Vega"
+  end 
+get '/favorite-song' do
+"My favorite song is Versace on the floor"
+end 
 end
